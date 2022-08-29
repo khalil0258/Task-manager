2d08f840164c60d3f9cb67365a207009b1c9a8a0
khalil0258 <brahim13khalil@gmail.com>
2022-08-29 13:24:45 +0100

    done with this mern stack app

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-// const connectionString = `mongodb+srv://brahim:brahimtayer@cluster0.kzskgcq.mongodb.net/?retryWrites=true&w=majority`;
 const connectDb = (url) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
